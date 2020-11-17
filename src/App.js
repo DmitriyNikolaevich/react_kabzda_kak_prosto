f489ad6e8ea74be7fcd3685cbd81e028f985d639
DmitriyNikolaevich <dnkaplun1987@gmail.com>
2020-11-17 23:44:13 +0300

    lazy suspend deploy almost

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import './App.css';
 import Nav from './components/Nav/Nav';
-import DialogsContainer from './components/Dialogs/DialogsContainer';
+//import DialogsContainer from './components/Dialogs/DialogsContainer';
 import News from './components/News/News';
 import Music from './components/Music/Music';
 import Settings from './components/Settings/Settings';
 import { Route } from 'react-router-dom';
-import UsersContainer from './components/users/UsersContainer';
-import ProfileContainer from './components/Profile/ProfileContainer';
+//import UsersContainer from './components/users/UsersContainer';
+//import ProfileContainer from './components/Profile/ProfileContainer';
 import HeaderContainer from './components/Header/HeaderContainer';
 import Login from './components/Login/Login';
 import { initializeThunk } from './redux/appReducer';
 import { connect } from 'react-redux';
 import Preloader from './components/common/preloader/Preloader';
+import { withSuspend } from './hoc/withSuspense';
+
+const DialogsContainer = React.lazy( () => import('./components/Dialogs/DialogsContainer'));
+const UsersContainer = React.lazy( () => import('./components/users/UsersContainer'));
+const ProfileContainer = React.lazy( () => import('./components/Profile/ProfileContainer'));
 
 
 class App extends React.Component {
@@ -30,9 +35,13 @@ class App extends React.Component {
         <HeaderContainer />
         <Nav />
         <div className='content'>
-          <Route render={() => <ProfileContainer />} path='/profile/:userID?' />
-          <Route render={() => <DialogsContainer />} path='/dialogs' />
-          <Route render={() => <UsersContainer />} path='/users' />
+
+          <Route render={withSuspend(ProfileContainer)} path='/profile/:userID?' />
+
+          <Route render={withSuspend(DialogsContainer)} path='/dialogs' />
+
+          <Route render={withSuspend(UsersContainer)} path='/users' />
+
           <Route component={News} path='/news' />
           <Route component={Music} path='/music' />
           <Route component={Settings} path='/settings' />
