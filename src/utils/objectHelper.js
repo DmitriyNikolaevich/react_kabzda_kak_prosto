a027874e14d4bce71333e11245ffb0f6a9ffe35e
DmitriyNikolaevich <dnkaplun1987@gmail.com>
2020-11-18 21:19:54 +0300

    Updates

--- src/utils/objectHelper.js
+++
@@ -1,9 +0,0 @@
-export const updateObjectInArray = (items, itemID, objPropName, newObjProps) => {
-    
-    items.map(u => {
-        if (u[objPropName] === itemID) {
-            return { ...u, ...newObjProps }
-        }
-        return u;
-    })
-}
